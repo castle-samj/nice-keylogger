3ca42548236816af13c9ef19fb8a4bcf82ebb811
Sam Castle <castles2@vcu.edu>
2023-04-22 12:31:36 -0400

    add cd to correct dir so systemd can launch correctly

--- src/restart.sh
+++ src/restart.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # used to restart the process and loop the "report"
 
+cd /home/pi/nicekl/
 sudo python3 controller.py &
 
 while true
 do
   if [ -s out.txt ]; then
+    # this loop is processed if the out.txt file has greater-than 0 size
     stolen_file=$(date +"%s.txt")
+    # creates a unique value, renaming out.txt
     mv out.txt "$stolen_file"
     stolen_content=$(cat "$stolen_file")
+    # launches the client TCP application while passing the contents of the file
     sudo python3 client.py "$stolen_content"
     rm -f "$stolen_file"
   fi
+  # repeat this loop ever x seconds
   sleep 10
 done
